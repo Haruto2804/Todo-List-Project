7871cef1e11d5daa00a235281b94279b78eeadf0
= <=>
2025-11-23 21:53:09 +0700

    feat (taskviews): create sort select, and add update and delete btn in ACTION col

--- Todo-List/src/pages/Tasks/TasksView.jsx
+++ Todo-List/src/pages/Tasks/TasksView.jsx
@@ -1,4 +1,6 @@
 import { useLocation } from 'react-router-dom'
+import { GoPencil } from "react-icons/go";
+import { FaRegTrashAlt } from "react-icons/fa";
 export function TasksView({ handleToggleCompleted, todo }) {
   console.log(todo);
   const location = useLocation();
@@ -51,14 +53,58 @@ export function TasksView({ handleToggleCompleted, todo }) {
 
   return (
     <>
-      <div className="ml-[258px] mr-80 flex-1 p-8 flex flex-col gap-5 overflow-y-auto">
+      <div className="ml-[250px] mr-79 flex-1 p-8 flex flex-col gap-5 overflow-y-auto">
         <div className="">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-5">
             <p className="text-3xl font-bold">{currentView === "completed" ?
               "Completed's Tasks" : currentView === "upcoming" ? "Upcoming's Tasks"
                 : currentView === "today" ? "Today's Tasks" : "All Tasks"
             }</p>
             <p className="text-gray-500 text-lg">{dateString}</p>
+          </div>
+
+        </div>
+        <div>
+          <div className="sort-controls flex gap-5 font-semibold">
+            <select className="
+            bg-gray-100    
+              border           
+            border-gray-300 
+            text-gray-700    
+              text-sm
+              rounded-md   
+              px-4           
+              py-2             
+              shadow-sm        
+              focus:ring-2     
+              focus:ring-blue-500
+              outline-none
+              cursor-pointer
+              appearance-none">
+              <option className="">Due Date (Ascending)</option>
+              <option className="" cvalue="due_date_desc">Due Date (Descending)</option>
+            </select>
+
+            <select className="
+              p-2 
+            bg-gray-100     
+              border           
+            border-gray-300 
+            text-gray-700    
+              text-sm       
+              rounded-md   
+              px-4           
+              py-2             
+              shadow-sm        
+              focus:ring-2     
+              focus:ring-blue-500
+              outline-none
+              cursor-pointer
+              appearance-none
+            ">
+              <option className="" value="priority_high">Priority (Ascending)</option>
+              <option className="" value="priority_low">Priority (Descending)</option>
+            </select>
           </div>
         </div>
         <div className="grid grid-cols-[auto,1fr,auto,auto,auto] border rounded-sm gap-5 border-gray-200">
@@ -72,18 +118,23 @@ export function TasksView({ handleToggleCompleted, todo }) {
           {filteredTasks.map((task) => {
             return (
               <div key={task.id} className=" cursor-pointer hover:bg-slate-100 transition-all col-span-5 grid grid-cols-subgrid items-center border-b border-gray-200 p-2">
-                <input disable = {task.completed} type="checkbox" className="size-5 cursor-pointer" />
-                <p className = {task.completed === true ? "line-through opacity-30" :""}>{task.todo}</p>
-                <p className = {task.completed === true ? "line-through opacity-30" :""}>{task.date}</p>
+                <input disable={task.completed} type="checkbox" className="size-5 cursor-pointer" />
+                <p className={task.completed === true ? "line-through opacity-30" : ""}>{task.todo}</p>
+                <p className={task.completed === true ? "line-through opacity-30" : ""}>{task.date}</p>
                 <p className={task.priority === "High" ? " text-red-800 bg-red-200 font-medium rounded-xl text-center p-2 " :
                   task.priority === "Medium" ? "text-yellow-800 bg-yellow-200 font-medium rounded-xl text-center p-2" :
                     task.priority === "Low" ? "text-gray-800 bg-gray-200 font-medium rounded-xl text-center p-2"
-                      :  "text-green-800 bg-green-200 font-medium rounded-xl text-center p-2"
+                      : "text-green-800 bg-green-200 font-medium rounded-xl text-center p-2"
 
                 }>{task.priority}</p>
-                <div className="flex flex-col opacity-0 hover:opacity-100">
-                  <p>Update</p>
-                  <p>Delete</p>
+                <div className="flex flex-col opacity-0 hover:opacity-100 items-center">
+                  <div className="p-2 hover:bg-black/10 transition-all rounded-full">
+                    <GoPencil />
+                  </div>
+                  <div className="p-2 hover:bg-black/10 transition-all rounded-full">
+                    <FaRegTrashAlt />
+                  </div>
+
                 </div>
               </div>
             )
